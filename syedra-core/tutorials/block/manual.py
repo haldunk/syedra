f6159df61fcefc909bc788b55a90fdf46806ade7
Haldun Komsuoglu <haldun.komsuoglu@robolit.com>
2024-10-26 11:47:06 +0300

    Manual port manipulation procedure is established

---
+++ syedra-core/tutorials/block/manual.py
@@ -0,0 +1,31 @@
+from syedra.core.block import *
+
+
+class Manual(Block):
+
+  value = OutputPort(initial=0.0)
+  
+
+class Printer(Block):
+
+  value = InputPort(initial=0.0)
+
+  def update(self):
+    print(self.value)
+
+
+if __name__ == '__main__':
+
+  manual = Manual()
+  printer = Printer()
+
+  manual['value'] >> printer['value']
+
+  try:
+    while True:
+      manual.value = random.randint(0, 10)
+      Block.execute(manual)
+      sleep(1)
+  except KeyboardInterrupt:
+    pass
+    
